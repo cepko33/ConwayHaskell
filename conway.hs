994cde12c8de9e2188d3ce78e1b8b4f24375e4aa
Stefan Cepko <stefan@localhost.localdomain>
2013-08-06 17:12:07 -0400

    fixed genRender to be less crazy

--- conway.hs
+++ conway.hs
@@ -23,36 +23,39 @@ numNeigh x y grid = sum [gp (x+n) (y+m) grid | n<-[-1..1], m<-[-1..1], not $ (n 
 
 beacon = [[0,0,1,1,0],[0,0,0,1,0],[1,0,0,0,0],[1,1,0,0,0],[0,0,0,0,0]]
 toad = [[0,1,0,0,0],[0,1,1,0,0],[0,1,1,0,0],[0,0,1,0,0],[0,0,0,0,0]]
-genGrid x = [[0 | n<-[1..x]] | m<-[1..x]]
+genGrid x y = [[0 | n<-[1..x]] | m<-[1..y]]
 
 
-updateGridCol x y grid
-	| y == (fromIntegral . length) grid = []
-	| gp x y grid == 1 = updateLive x y grid: updateGridCol x (y+1) grid
-	| gp x y grid == 0 = updateDead x y grid: updateGridCol x (y+1) grid
-
-updateGrid x grid
+updateGridRow x y grid
 	| x == (fromIntegral . length) grid = []
-	| otherwise = updateGridCol x 0 grid:updateGrid (x+1) grid
+	| gp x y grid == 1 = updateLive x y grid: updateGridRow (x+1) y grid
+	| gp x y grid == 0 = updateDead x y grid: updateGridRow (x+1) y grid
 
-genRender siz grid = genRenderW 0 siz grid
+updateGrid y grid
+	| y == (fromIntegral . length) grid = []
+	| otherwise = updateGridRow 0 y grid:updateGrid (y+1) grid
 
-genRenderW _ _ [] = []
-genRenderW x siz grid = genRender' x 0 siz (head grid) ++ genRenderW (x+1) siz (tail grid)
+genRender siz grid = gr 0 siz grid
+	where
+	gr _ _ [] = []
+	gr x siz (c:cs) = genRender' x 0 siz c ++ gr (x+1) siz cs
 
 genRender' _ _ _ [] = []
 genRender' x y siz (0:rest) = genRender' x (y+1) siz rest
-genRender' x y siz (1:rest) = (move (siz*x + siz/2, siz*y + siz/2) $ filled red $ rect siz siz):genRender' x (y+1) siz rest
+genRender' x y siz (1:rest) = newRect:genRender' x (y+1) siz rest
+	where 
+	coord = (siz*x + siz/2, siz*y + siz/2) 
+	newRect = (move coord $ filled red $ rect siz siz)	
 
 step :: Time -> [[Integer]] -> [[Integer]]
-step dt True grid = updateGrid 0 grid
+step dt grid = updateGrid 0 grid
 
 fi = fromIntegral
 
 render :: [[Integer]] -> (Int,Int) -> Element
-render grid (w,h) = collage w h (genRender 20 grid)
+render grid (w,h) = collage w h (genRender 30 grid)
 
 main :: IO ()
-main = run defaultConfig $ render <~ foldp step toad ( (Time.delay $ Time.fps 2)  ~~ Window.dimensions 
+main = run defaultConfig $ render <~ foldp step beacon (Time.delay $ Time.fps 20)  ~~ Window.dimensions 
 
 
